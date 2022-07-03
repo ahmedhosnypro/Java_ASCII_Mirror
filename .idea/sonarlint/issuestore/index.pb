4e2e807b2b41e471ef5b94017bb1d704edd4f8fa
ahmedhosnypro <ahhosnyas@gmail.com>
2022-07-03 03:23:51 +0200

    New friends

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+V
+&ASCII Mirror/task/test/MirrorTest.java,f\2\f2029125202fe8a14b58e218923b13d1c77a2c73
+Z
+*ASCII Mirror/task/src/asciimirror/text.txt,4\2\42ccc3c2874b0395a050c2612a1506b7db5ef973
--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -2,28 +2,21 @@ package asciimirror;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> lines = new ArrayList<>();
 
         System.out.println("Input the file path:");
         Scanner scanner = new Scanner(System.in);
         String filePath = scanner.nextLine();
-        System.out.println(filePath);
 
-        try (Scanner fileScanner = new Scanner(new File("D:\\7-Learn\\Java\\HyperSkill\\ASCII Mirror\\ASCII Mirror\\task\\src\\asciimirror\\text.txt"))) {
+        try (Scanner fileScanner = new Scanner(new File(filePath))) {
             while (fileScanner.hasNext()) {
-                lines.add(fileScanner.nextLine());
+                System.out.println(fileScanner.nextLine());
             }
-
-            var length = lines.stream().sorted(Comparator.comparingInt(String::length).reversed()).findFirst().get().length();
-            lines.forEach(l -> System.out.println(l + " ".repeat(length - l.length())));
         } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            System.out.println("File not found!");
         }
     }
 }
